bb68c155fceb8036e40cc88893edaf848fdf8e97
admincdy <admincdy>
2016-06-13 23:57:04 +0800

    播放器创建

---
+++ xmplay/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace xmplay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.wmp1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.wmp1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开文件(&o)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "音频文件|*.mp3;*.wma;*.wav|所有文件|*.*";
+            this.openFileDialog1.Title = "请选择音频文件";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // wmp1
+            // 
+            this.wmp1.AccessibleName = "播放器";
+            this.wmp1.Enabled = true;
+            this.wmp1.Location = new System.Drawing.Point(8, 19);
+            this.wmp1.Name = "wmp1";
+            this.wmp1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmp1.OcxState")));
+            this.wmp1.Size = new System.Drawing.Size(75, 23);
+            this.wmp1.TabIndex = 1;
+            this.wmp1.TabStop = false;
+            this.wmp1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(214, 48);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "播放(&p)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(112, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "停止(&s)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(42, 92);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "音量加(&u)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(194, 92);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "音量减(&d)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(42, 121);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(146, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "快进(&j)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(224, 121);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(243, 10);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "快退(&k)";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.wmp1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "熊猫音乐播放器";
+            ((System.ComponentModel.ISupportInitialize)(this.wmp1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private AxWMPLib.AxWindowsMediaPlayer wmp1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ xmplay/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace xmplay
+{
+    public partial class Form1 : Form
+    {
+        public string filename;//保存音乐文件路径
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //打开选择文件对话框
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {       
+            //将用户选择的文件路径副职给播放器
+            wmp1.URL = openFileDialog1.FileName;
+            button2.Text = "暂停(&p)";//更改按钮标题。
+            this.Text = "熊猫音乐播放器" + " - 正在播放："+wmp1.currentMedia.getItemInfo("title");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (wmp1.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                wmp1.Ctlcontrols.pause();
+                button2.Text = "播放(&p)";
+
+            }
+            else
+            {
+                wmp1.Ctlcontrols.play();
+                button2.Text = "暂停(&p)";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            wmp1.Ctlcontrols.stop();
+            button2.Text = "播放(&p)";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            wmp1.settings.volume = wmp1.settings.volume + 3;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+                if (wmp1.settings.volume>9)
+            {
+                wmp1.settings.volume = wmp1.settings.volume - 2;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            wmp1.Ctlcontrols.currentPosition = wmp1.Ctlcontrols.currentPosition + 3;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            wmp1.Ctlcontrols.currentPosition = wmp1.Ctlcontrols.currentPosition -3;
+        }
+    }
+}
